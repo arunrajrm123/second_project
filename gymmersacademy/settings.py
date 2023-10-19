6a6f7ddd5f55e0e53584c0671ce36da0f262672c
arun <arunrajrmvnr@gmail.com>
2023-10-19 18:13:14 +0530

    monitor

--- gymmersacademy/settings.py
+++ gymmersacademy/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-*2wv8%z+kkc4wblws$lksr%z_oc5k%5)4lwcw0$^)7=_$d1=ki
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
