e6cbe12169f241ddbf634321551422c00d1c3537
arun <arunrajrmvnr@gmail.com>
2023-10-20 22:00:38 +0530

    cv

--- gymmersacademy/deployment.py
+++ gymmersacademy/deployment.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = [os.environ['SECRET']]
+SECRET_KEY = "U0QvBVIRjG"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
